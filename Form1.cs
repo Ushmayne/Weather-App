8cf306889133160a152e9652f39e594205920611
usman <usmannaveed012@gmail.com>
2025-07-05 17:44:03 -0400

    refactores some code to remove repeating lines and added comments

--- Form1.cs
+++ Form1.cs
@@ -27,7 +27,7 @@ public partial class Form1 : Form
 
     private void displayOptions()
     {
-        cityTextBox = new TextBox();
+        cityTextBox = new TextBox();//declaring all the labels and textboxes
         checkWeatherButton = new Button();
         condition = new Label();
         detail = new Label();
@@ -35,12 +35,102 @@ public partial class Form1 : Form
         sunset = new Label();
         windSpeed = new Label();
         pressure = new Label();
+ 
+
+        clearOptions(); // Reset all options to their defaults
+
+        this.Controls.Add(cityTextBox);
+        this.Controls.Add(checkWeatherButton);
+        this.Controls.Add(condition);
+        this.Controls.Add(detail);
+        this.Controls.Add(sunrise);
+        this.Controls.Add(sunset);
+        this.Controls.Add(windSpeed);
+        this.Controls.Add(pressure);
 
 
+
+        cityTextBox.Enabled = true;//making sure the textbox is enabled and readable
+        cityTextBox.ReadOnly = false;
+
+
+
+
+    }
+
+    private void CheckWeatherButton_Click(object sender, EventArgs e)
+    {
+        // Get the city name from the TextBox
+        Console.WriteLine(cityTextBox.Text.ToString());
+        string city = cityTextBox.Text.Trim();
+
+        if (string.IsNullOrEmpty(city))
+        {
+            MessageBox.Show("Please enter a city name.");
+            clearOptions(); // Clear the TextBox from any previous input
+            return;
+        }
+        else
+        {
+            getWeather();
+        }
+
+    }
+
+    void getWeather()
+    {
+        using (WebClient client = new WebClient())
+        {
+            string url = String.Format($"http://api.openweathermap.org/data/2.5/weather?q={cityTextBox.Text}&appid={apiKey}&units=metric");//url for openweather api
+
+            using (HttpClient client1 = new HttpClient())
+            {
+                try//to make sure of a succesful connection
+                {
+
+                    HttpResponseMessage response = client1.GetAsync(url).Result;
+                    response.EnsureSuccessStatusCode();
+
+                    string content = response.Content.ReadAsStringAsync().Result;  // Get response content synchronously
+                    JObject weatherJson = JObject.Parse(content);
+                    if (weatherJson["cod"].ToString() == "200")//200 means the request was successful
+                    {
+                        var json = client.DownloadString(url);
+                        weatherData.root info = JsonConvert.DeserializeObject<weatherData.root>(json);
+                        condition.Text = info.weather[0].main;
+                        detail.Text = info.weather[0].description;
+                        sunset.Text = convertDateTime(info.sys.sunset).ToString();
+                        sunrise.Text = convertDateTime(info.sys.sunrise).ToString();
+                        windSpeed.Text = info.wind.speed.ToString() + " m/s";
+                        pressure.Text = info.main.pressure.ToString() + " hPa";
+                    }
+
+
+                }
+                catch (Exception ex)//catch exceptions if they enter the wrong city name
+                {
+                    Console.WriteLine("Error: " + ex.Message);//writes error message to the console
+                    clearOptions(); // Clear the TextBox from any previous input
+                }
+            }
+
+
+        }
+    }
+
+    DateTime convertDateTime(long milisec)//convert milliseconds into proper date time format
+    {
+        DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
+        return (day.AddMilliseconds(milisec).ToLocalTime());
+
+    }
+
+    void clearOptions()//reset all the options to their defaults
+    {
 
         Font font = new Font("Arial", 12, FontStyle.Regular);
 
-        //city textbox
+        //city textbox intializing
         cityTextBox.Location = new Point(110, 70);
         cityTextBox.Size = new Size(200, 80);
         cityTextBox.PlaceholderText = "Enter city name";
@@ -87,109 +177,6 @@ public partial class Form1 : Form
         pressure.Text = "N/A";
         pressure.Font = font;
         pressure.BackColor = Color.Transparent;
-
-
-
-        this.Controls.Add(cityTextBox);
-        this.Controls.Add(checkWeatherButton);
-        this.Controls.Add(condition);
-        this.Controls.Add(detail);
-        this.Controls.Add(sunrise);
-        this.Controls.Add(sunset);
-        this.Controls.Add(windSpeed);
-        this.Controls.Add(pressure);
-
-
-
-        cityTextBox.Enabled = true;//making sure the textbox is enabled and readable
-        cityTextBox.ReadOnly = false;
-
-
-
-
-    }
-
-    private void CheckWeatherButton_Click(object sender, EventArgs e)
-    {
-        // Get the city name from the TextBox
-        Console.WriteLine(cityTextBox.Text.ToString());
-        string city = cityTextBox.Text.Trim();
-
-        if (string.IsNullOrEmpty(city))
-        {
-            MessageBox.Show("Please enter a city name.");
-            cityTextBox.Text = string.Empty; // Clear the TextBox from any previous input
-            return;
-        }
-        else
-        {
-            getWeather();
-        }
-
-        // Make API call to get weather data
-        //string weatherInfo = await GetWeatherDataAsync(city);
-
-        // Display the result
-        //resultLabel.Text = weatherInfo;
-    }
-
-    void getWeather()
-    {
-        using (WebClient client = new WebClient())
-        {
-            string url = String.Format($"http://api.openweathermap.org/data/2.5/weather?q={cityTextBox.Text}&appid={apiKey}&units=metric");
-
-            using (HttpClient client1 = new HttpClient())
-            {
-                try//to make sure of a succesful connection
-                {
-
-                    HttpResponseMessage response = client1.GetAsync(url).Result;
-                    response.EnsureSuccessStatusCode();
-
-                    string content = response.Content.ReadAsStringAsync().Result;  // Get response content synchronously
-                    JObject weatherJson = JObject.Parse(content);
-                    if (weatherJson["cod"].ToString() == "200")
-                    {
-                        var json = client.DownloadString(url);
-                        weatherData.root info = JsonConvert.DeserializeObject<weatherData.root>(json);
-                        condition.Text = info.weather[0].main;
-                        detail.Text = info.weather[0].description;
-                        sunset.Text = convertDateTime(info.sys.sunset).ToString();
-                        sunrise.Text = convertDateTime(info.sys.sunrise).ToString();
-                        windSpeed.Text = info.wind.speed.ToString() + " m/s";
-                        pressure.Text = info.main.pressure.ToString() + " hPa";
-                    }
-
-
-                }
-                catch (Exception ex)//catch exceptions if they enter the wrong city name
-                {
-                    Console.WriteLine("Error: " + ex.Message);
-                    clearOptions(); // Clear the TextBox from any previous input
-                }
-            }
-
-
-        }
-    }
-
-    DateTime convertDateTime(long milisec)
-    {
-        DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
-        return (day.AddMilliseconds(milisec).ToLocalTime());
-
-    }
-
-    void clearOptions()//reset all the options to their defaults
-    {
-        cityTextBox.Text = string.Empty; 
-        condition.Text = "Condition"; 
-        detail.Text = "Detail"; 
-        sunrise.Text = "N/A"; 
-        sunset.Text = "N/A"; 
-        windSpeed.Text = "N/A"; 
-        pressure.Text = "N/A"; 
     }
 
     
